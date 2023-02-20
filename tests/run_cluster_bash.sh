651b8e94434d8c1902a4d5db874b395802231fef
Zhewei <zchen15@github.com>
2023-02-20 14:30:30 -0800

    rebuild

--- tests/run_cluster_bash.sh
+++ tests/run_cluster_bash.sh
@@ -1,3 +1,3 @@
-CGDesigner -c data/npbop.json design -material rna -s terminator_switch -N 1 -gin data/mtrig.csv -o ts32_mtrig -d 10 20 6 0 7 3 1 -fstop 0.01 -scan 100 100
-CGDesigner -c data/npbop.json checkpoint -i ts32*.spec
+CGDesigner -c npbop.json design -material rna -s reverse_toehold_switch -N 1 -gin ../data/mtrig.csv -o ts45_mtrig -d 10 20 10 3 10 3 3 6 -fstop 0.01 -scan 100 100
+CGDesigner -c data/npbop.json checkpoint -i ts45*.spec
 
